998978135197610d900fc558b70057aa33f1b28c
Ambrose Bonnaire-Sergeant <ambrose@ambrosebs.com>
2024-02-06 18:15:11 -0600

    ideas

--- src/clj_reload/core.clj
+++ src/clj_reload/core.clj
@@ -25,41 +25,49 @@
 ;
 ;  Namespace :: {:depends #{<symbol> ...}}
 
-(def *state
+(def ^:private *state
   (atom {}))
 
-(def reader-opts
+(def ^:private reader-opts
   {:read-cond :allow
-   :features  #{:clj}
-   :eof       ::eof})
+   :features  #{:clj}})
 
-(defn update! [m k f & args]
-  (assoc! m k (apply f (m k) args)))
+(defn- update!
+  ([m k f]
+   (assoc! m k (f (m k))))
+  ([m k f x]
+   (assoc! m k (f (m k) x)))
+  ([m k f x y]
+   (assoc! m k (f (m k) x y)))
+  ([m k f x y & args]
+   (assoc! m k (apply f (m k) x y args))))
 
-(def conjs
+(def ^:private conjs
   (fnil conj #{}))
 
-(def intos
+(def ^:private intos
   (fnil into #{}))
 
-(defn now []
+(defn- now []
   (System/currentTimeMillis))
 
-(defn last-modified [^File f]
+(defn- last-modified [^File f]
   (some-> f .lastModified))
 
-(defn file? [^File f]
+(defn- file? [^File f]
   (some-> f .isFile))
 
-(defn file-name [^File f]
+(defn- file-name [^File f]
   (some-> f .getName))
 
-(defn reader ^PushbackReader [f]
+(defn- reader ^PushbackReader [f]
   (PushbackReader. (io/reader (io/file f))))
 
 (defn- expand-quotes [form]
   (walk/postwalk
-    #(if (and (sequential? %) (not (vector? %)) (= 'quote (first %)))
+    #(if (and (seq? %)
+              (= 2 (count %))
+              (= 'quote (first %)))
        (second %)
        %)
     form))
@@ -67,118 +75,120 @@
 (defn parse-require-form [form]
   (loop [body   (next form)
          result #{}]
-    (let [[decl & body'] body]
-      (cond
-        (empty? body)
-        result
-        
-        (symbol? decl) ;; a.b.c
-        (recur body' (conj result decl))
-        
-        (not (sequential? decl))
-        (do
-          (println "Unexpected" (first form) "form:" (pr-str decl))
-          (recur body' result))
-        
-        (not (symbol? (first decl)))
-        (do
-          (println "Unexpected" (first form) "form:" (pr-str decl))
-          (recur body' result))
-        
-        (or
-          (nil? (second decl))      ;; [a.b.d]
-          (keyword? (second decl))) ;; [a.b.e :as e]
-        (recur body' (conj result (first decl)))
-        
-        :else ;; [a.b f [g :as g]]
-        (let [prefix   (first decl)
-              suffixes (map #(if (symbol? %) % (first %)) (next decl))]
-          (recur body' (into result (map #(symbol (str (name prefix) "." (name %))) suffixes))))))))
+    (if (empty? body)
+      result
+      (let [[decl & body'] body]
+        (cond
+          (symbol? decl) ;; a.b.c
+          (recur body' (conj result decl))
+
+          (not (sequential? decl))
+          (do
+            (println "Unexpected" (first form) "form:" (pr-str decl))
+            (recur body' result))
+
+          (not (symbol? (first decl)))
+          (do
+            (println "Unexpected" (first form) "form:" (pr-str decl))
+            (recur body' result))
+
+          (or
+            (nil? (second decl))      ;; [a.b.d]
+            (keyword? (second decl))) ;; [a.b.e :as e]
+          (recur body' (conj result (first decl)))
+
+          :else ;; [a.b f [g :as g]]
+          (let [prefix   (first decl)]
+            (recur body' (into result
+                               (comp ;; suffixes
+                                     (map #(if (symbol? %) % (first %)) (next decl))
+                                     (map #(symbol (str (name prefix) "." (name %)))))
+                               (next decl)))))))))
 
 (defn parse-ns-form [form]
   (let [name (second form)
+        _ (assert (simple-symbol? name) (str "Invalid ns form: " (pr-str form)))
         body (loop [body   (nnext form)
                     result {}]
-               (let [[form & body'] body
-                     tag  (when (list? form)
-                            (first form))]
-                 (cond
-                   (empty? body)
-                   result
-          
-                   (#{:require :use} tag)
-                   (recur body' (update result :depends intos (parse-require-form form)))
-          
-                   :else
-                   (recur body' result))))]
+               (if (empty? body)
+                 result
+                 (let [[form & body'] body
+                       tag  (when (list? form)
+                              (first form))]
+                   (if (#{:require :use} tag)
+                     (recur body' (update result :depends intos (parse-require-form form)))
+                     (recur body' result)))))]
     [name body]))
 
 (defn read-file
   "Returns {<symbol> -> Namespace}"
   [rdr]
-  (loop [current-ns nil
-         result     {}]
-    (let [form (binding [*read-eval* false]
-                 (read reader-opts rdr))
-          tag  (when (list? form)
-                 (first form))]
-      (cond
-        (= ::eof form)
+  (let [eof (Object.)
+        reader-opts (assoc reader-opts :eof eof)]
+    (loop [current-ns nil
+           result     {}]
+      (if (identical? eof form)
         result
-          
-        (= 'ns tag)
-        (let [[name body] (parse-ns-form form)]
-          (recur name (assoc result name body)))
-          
-        (= 'in-ns tag)
-        (let [[_ name] (expand-quotes form)]
-          (recur name result))
-          
-        (#{'require 'use} tag)
-        (let [_    (assert current-ns (str "Unexpected " tag " form outside of ns"))
-              deps (parse-require-form (expand-quotes form))]
-          (recur current-ns (update result current-ns update :depends intos deps)))
-            
-        :else
-        (recur current-ns result)))))
+        (let [form (binding [*read-eval* false]
+                     (read reader-opts rdr))
+              tag  (when (seq? form)
+                     (first form))]
+          (case tag
+            ns
+            (let [[name body] (parse-ns-form form)]
+              (recur name (assoc result name body)))
+
+            in-ns
+            (let [_        (assert (= 2 (count form)) (str "Invalid in-ns form: " (pr-str form)))
+                  [_ name] (expand-quotes form)]
+              (assert (simple-symbol? name) (str "Invalid in-ns form: " (pr-str form)))
+              (recur name result))
+
+            (require use)
+            (let [_    (assert current-ns (str "Unexpected " tag " form outside of ns"))
+                  deps (parse-require-form (expand-quotes form))]
+              (recur current-ns (update result current-ns update :depends intos deps)))
+
+            (recur current-ns result)))))))
 
 (defn find-files [dirs since]
-  (->> dirs
-    (mapcat #(file-seq (io/file %)))
-    (filter file?)
-    (filter #(re-matches #".*\.cljc?" (file-name %)))
-    (filter #(> (last-modified %) (or since 0)))))
+  (sequence
+    (comp (mapcat #(file-seq (io/file %)))
+          (filter file?)
+          (filter #(re-matches #".*\.cljc?" (file-name %)))
+          (filter #(> (last-modified %) (or since 0))))
+    dirs))
 
 (defn dependees 
   "ns -> #{downstream-ns ...}"
   [files]
-  (let [*m (volatile! (transient {}))]
-    (doseq [[_ {nses :namespaces}] files
-            [from {tos :depends}] nses]
-      (vswap! *m update! from #(or % #{}))
-      (doseq [to tos]
-        (vswap! *m update! to conjs from)))
-    (persistent! @*m)))
+  (->> files
+       (reduce (fn [acc [_ {nses :namespaces}]]
+                 (reduce (fn [acc [from {tos :depends}]]
+                           (reduce (fn [acc to]
+                                     (update! acc to conjs from))
+                                   (update! acc from #(or % #{}))
+                                   tos))
+                         acc
+                         nses))
+               (transient {}))
+       persistent!))
 
 (defn transitive-closure
-  "Starts from starts, expands using dependees {ns -> #{downsteram-ns ...}},
+  "Starts from starts, expands using dependees {ns -> #{downstream-ns ...}},
    returns #{ns ...}"
   [deps starts]
   (loop [queue starts
          acc   (transient #{})]
-    (let [[start & queue'] queue]
-      (cond
-        (empty? queue)
-        (persistent! acc)
-      
-        (contains? acc start)
-        (recur queue' acc)
-        
-        :else
-        (recur (into queue (deps start)) (conj! acc start))))))
+    (if (empty? queue)
+      (persistent! acc)
+      (let [[start & queue'] queue]
+        (if (contains? acc start)
+          (recur queue' acc)
+          (recur (into queue (deps start)) (conj! acc start)))))))
 
 (defn topo-sort-fn
-  "Accepts dependees map {ns -> #{downsteram-ns ...}},
+  "Accepts dependees map {ns -> #{downstream-ns ...}},
    returns a fn that topologically sorts dependencies"
   [deps]
   (let [sorted (loop [res  (transient [])
@@ -186,7 +196,7 @@
                  (if (empty? deps)
                    (persistent! res)
                    (let [root (fn [node]
-                                (when (every? #(not (% node)) (vals deps))
+                                (when (not-any? #(% node) (vals deps))
                                   node))
                          node (if *stable?*
                                 (->> (keys deps) (filter root) (sort) (first))
@@ -198,18 +208,21 @@
 (defn changed-files
   "Returns {<file> -> File}"
   [dirs since]
-  (let [changed-files (find-files dirs since)]
-    (into {}
-      (for [file changed-files]
-        [file {:modified (last-modified file)
-               :namespaces
-               (with-open [rdr (reader file)]
-                 (try
-                   (read-file rdr)
-                   (catch Exception e
-                     (printf "Failed to load %s: %s\n" (.getPath ^File file) (.getMessage e)))))}]))))
+  (->> (find-files dirs since)
+       (reduce
+         (fn [acc changed-file]
+           (assoc! acc changed-file
+                   {:modified (last-modified changed-file)
+                    :namespaces
+                    (with-open [rdr (reader changed-file)]
+                      (try
+                        (read-file rdr)
+                        (catch Exception e
+                          (printf "Failed to load %s: %s\n" (.getPath ^File changed-file) (.getMessage e)))))}))
+         (transient {}))
+       persistent!))
 
-(defn init-impl [opts]
+(defn- init-impl [opts]
   (let [dirs  (vec (:dirs opts))
         now   (now)
         files (changed-files dirs 0)]
@@ -219,20 +232,20 @@
      :files     files
      :since     now}))
 
-(defn init [opts]
-  (reset! *state (init-impl opts)))
+(defn init
+  ([opts] (init *state opts))
+  ([*state opts]
+   (reset! *state (init-impl opts))))
 
-(defn scan-impl [state]
+(defn- scan-impl [state]
   (let [{:keys [dirs since load unload no-unload no-load files]} state
         now           (now)
         changed-files (changed-files dirs since)
-        since'        (->> changed-files
-                        vals
-                        (map :modified)
-                        (reduce max now))
-        changed-nses  (for [[_ {nses :namespaces}] changed-files
-                            [ns _] nses]
-                        ns)
+        since'        (reduce (fn [n [_ {:keys [modified]}]]
+                                (max n modified))
+                              now changed-files)
+        changed-nses  (mapcat (comp keys :namespaces val)
+                              changed-files)
         loaded        @@#'clojure.core/*loaded-libs*
         
         unload?       #(and
@@ -252,7 +265,7 @@
         load?         #(and
                          (loaded %)
                          (not (no-load %)))
-        files'        (merge files changed-files)
+        files'        (into files changed-files)
         dependees'    (clj-reload.core/dependees files')
         topo-sort'    (topo-sort-fn dependees')
         load'         (->> changed-nses
@@ -301,18 +314,27 @@
     (assoc state
       :load (next load))))
 
-(defn reload []
-  (swap! *state scan-impl)
-  (loop [state @*state]
-    (cond
-      (not-empty (:unload state))
-      (recur (swap! *state unload-impl))
-      
-      (not-empty (:load state))
-      (recur (swap! *state load-impl))
-    
-      :else
-      state)))
+(defn- swap-once! [a state f msg]
+  (let [state' (f state)]
+    (if (compare-and-set! *state state state')
+      state'
+      (throw (ex-info (str "*state changed during " msg)
+                      {:expected-state state
+                       :actual-state @a})))))
+
+(defn reload
+  ([] (reload *state))
+  ([*state]
+   (loop [state (swap-once! *state @state scan-impl "scan-impl")]
+     (cond
+       (seq (:unload state))
+       (recur (swap-once! *state state unload-impl "unload-impl"))
+
+       (seq (:load state))
+       (recur (swap-once! *state state load-impl "load-impl"))
+
+       :else
+       state))))
 
 (comment
   (dependencies (first-scan ["fixtures"]))
